0bb2448b307b848392140c8c627b97e0dfe17ae1
Sebastián <sebachamena@gmail.com>
2020-05-31 15:16:19 -0300

    Add tree structure to semprov.Category

--- semprov/category.go
+++ semprov/category.go
@@ -1,10 +1,24 @@
 package semprov
 
-type Category string
+type Category struct {
+	Name   string
+	Childs []*Category
+}
 
-const (
-	Psychologist Category = "psychologist"
-	Medic        Category = "medic"
-	Lawyer       Category = "lawyer"
-	Attorney     Category = "attorney"
+var (
+	Root = &Category{Name: "root", Childs: []*Category{
+		Health, Justice,
+	}}
+
+	Health = &Category{Name: "health", Childs: []*Category{
+		Psychologist, Medic,
+	}}
+	Psychologist = &Category{Name: "psychologist"}
+	Medic        = &Category{Name: "medic"}
+
+	Justice = &Category{Name: "justice", Childs: []*Category{
+		Lawyer, Attorney,
+	}}
+	Lawyer   = &Category{Name: "lawyer"}
+	Attorney = &Category{Name: "attorney"}
 )
